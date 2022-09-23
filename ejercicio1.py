c805e1cfff73576ae51bc02311742e0aecc20a13
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-23 16:16:20 +0200

    ejercicio 1 intentandolo

---
+++ ejercicio1.py
@@ -0,0 +1,21 @@
+def calculador(mdm,n):
+    suma=0
+    suma=mdm[n][0]+mdm[n][1]+mdm[n][2]
+    if(suma!=mdm[n][3]):
+        mdm[n][3]=suma
+    else:
+        return print('Fila correcta')
+
+
+
+def main():
+    
+    matriz=[ [1, 1, 1, 3],
+            [2, 2, 2, 7],
+            [3, 3, 3, 9],
+            [4, 4, 4, 13]]
+
+
+
+if __name__=='__main__':
+    main()
