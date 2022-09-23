6d00e4e7d8d6709f0c3a7f0e231673d159bb1936
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-23 16:55:13 +0200

    main con el 2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from ejs.ejercicio1 import *
+from ejs.ejercicio2 import *
 
 
 def main():
     qedv=int(input('Que ejercicio desea ver (1) (2) (3) (4) : '))
     if(qedv==1):
         main1()
+    if(qedv==2):
+        main2()
 
 
 
