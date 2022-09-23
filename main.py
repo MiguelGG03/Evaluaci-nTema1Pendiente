62b54f6550a186cf494604b7752f2580e8b13043
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-23 18:31:47 +0200

    para seguir mirando el programa

--- main.py
+++ main.py
@@ -5,15 +5,37 @@ from ejs.tabla import *
 
 
 def main():
-    qedv=int(input('Que ejercicio desea ver (1) (2) (3) (4) : '))
-    if(qedv==1):
-        main1()
-    if(qedv==2):
-        main2()
-    if(qedv==3):
-        main3()
-    if(qedv==4):
-        main4()
+    seguir=True
+    while(seguir==True):
+        qedv=int(input('Que ejercicio desea ver (1) (2) (3) (4) : '))
+        if(qedv==1):
+            main1()
+            desea=int(input('Desea seguir \n(0) No \n(1) Si\nRespuesta:'))
+            if(desea==0):
+                seguir=False
+            else:
+                seguir=True
+        elif(qedv==2):
+            main2()
+            desea=int(input('Desea seguir \n(0) No \n(1) Si\nRespuesta:'))
+            if(desea==0):
+                seguir=False
+            else:
+                seguir=True
+        elif(qedv==3):
+            main3()
+            desea=int(input('Desea seguir \n(0) No \n(1) Si\nRespuesta:'))
+            if(desea==0):
+                seguir=False
+            else:
+                seguir=True
+        elif(qedv==4):
+            main4()
+            desea=int(input('Desea seguir \n(0) No \n(1) Si\nRespuesta:'))
+            if(desea==0):
+                seguir=False
+            else:
+                seguir=True
 
 
 
