1a36b23fa1d6598b3039c04c9b5d9f3f4e88348f
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-09-23 16:24:29 +0200

    finalizado ej 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -2,9 +2,11 @@ def calculador(mdm,n):
     suma=0
     suma=mdm[n][0]+mdm[n][1]+mdm[n][2]
     if(suma!=mdm[n][3]):
+        antiguo=mdm[n][3]
         mdm[n][3]=suma
+        return print('Hemos cambiado el {} por un {} en la Fila {}. [{}, {}, {}, {}]'.format(str(antiguo),str(suma),str(n+1),mdm[n][0],mdm[n][1],mdm[n][2],mdm[n][3]))
     else:
-        return print('Fila {} correcta'.format(str(n+1)))
+        return print('Fila {} correcta. [{}, {}, {}, {}]'.format(str(n+1),mdm[n][0],mdm[n][1],mdm[n][2],mdm[n][3]))
 
 
 
@@ -16,7 +18,7 @@ def main1():
             [4, 4, 4, 13]]
     qfdm=int(input('Que fila desea comprobar: '))
     qfdm=qfdm-1
-    calculador(matriz,0)
+    calculador(matriz,qfdm)
 
 
 
